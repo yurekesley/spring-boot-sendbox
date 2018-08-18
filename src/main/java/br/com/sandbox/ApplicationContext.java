b5b0c21697a6546fb69b71aff7d38ac86dc241f4
Yure Kesley <yurekesley@gmail.com>
2018-08-17 23:12:16 -0300

    configurando spring para web application

--- src/main/java/br/com/sandbox/ApplicationContext.java
+++ src/main/java/br/com/sandbox/ApplicationContext.java
@@ -4,10 +4,9 @@ package br.com.sandbox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-public class BootApplication {
+public class ApplicationContext {
 	public static void main(String[] args) {
-		SpringApplication.run(BootApplication.class, args);
+		SpringApplication.run(ApplicationContext.class, args);
 	}
 }
--- src/main/java/br/com/sandbox/config/BDConfig.java
+++ src/main/java/br/com/sandbox/config/BDConfig.java
@@ -1,25 +1,9 @@
 package br.com.sandbox.config;
 
-import javax.sql.DataSource;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 @Configuration
 public class BDConfig {
-	
-	
-	@Bean
-	public DataSource dataSource() {
-		return new EmbeddedDatabaseBuilder()
-				.generateUniqueName(false)
-				.setName("boot")
-				.setType(EmbeddedDatabaseType.H2)
-				.setScriptEncoding("UTF-8")
-				.addScript("bd/script.sql")
-				.build();
-	}
+		
 	
 }
